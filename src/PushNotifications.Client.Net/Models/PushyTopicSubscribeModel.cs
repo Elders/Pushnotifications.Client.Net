f9f140f740de09d95de5e5b173271e109141aa71
AlexPlom <alexiosplomaritis@gmail.com>
2018-07-16 17:06:07 +0300

    Adds endpoints to support the topic subscription and sending to topic

---
+++ src/PushNotifications.Client.Net/Models/PushyTopicSubscribeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PushNotifications.Api.Client
+{
+    public class PushyTopicSubscribeModel
+    {
+        public PushyTopicSubscribeModel(string subscriberId, string topic)
+        {
+            if (ReferenceEquals(subscriberId, null) == true) throw new ArgumentNullException(nameof(subscriberId));
+            if (string.IsNullOrEmpty(topic) == true) throw new ArgumentNullException(nameof(topic));
+
+            SubscriberUrn = subscriberId;
+            Token = topic;
+        }
+
+        public string SubscriberUrn { get; private set; }
+
+        public string Token { get; private set; }
+    }
+}
---
+++ src/PushNotifications.Client.Net/Models/SendPushNotificationToTopicModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using PushNotifications.Contracts;
+
+namespace PushNotifications.Api.Client.Models
+{
+    public class SendPushNotificationToTopicModel
+    {
+        public SendPushNotificationToTopicModel(string topic, string tenant, string title, string body, string sound, string icon, int badge, DateTime expiresAtUtc, bool contentAvailable, Dictionary<string, object> notificationData)
+        {
+            if (topic is null) throw new ArgumentNullException(nameof(topic));
+            if (notificationData is null) throw new ArgumentNullException(nameof(notificationData));
+            if (ReferenceEquals(null, expiresAtUtc)) throw new ArgumentNullException(nameof(expiresAtUtc));
+
+            Topic = topic;
+            Tenant = tenant;
+            Title = title;
+            Body = body;
+            Sound = sound;
+            Icon = icon;
+            Badge = badge;
+            ExpiresAt = new Timestamp(expiresAtUtc);
+            ContentAvailable = contentAvailable;
+            NotificationData = notificationData;
+        }
+
+        public string Topic { get; private set; }
+
+        public string Tenant { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Body { get; private set; }
+
+        public string Sound { get; private set; }
+
+        public string Icon { get; private set; }
+
+        public int Badge { get; private set; }
+
+        public Timestamp ExpiresAt { get; private set; }
+
+        public bool ContentAvailable { get; private set; }
+
+        public Dictionary<string, object> NotificationData { get; private set; }
+
+    }
+}
