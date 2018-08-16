9d26e859157da9ad3ecf503c578040662a6bba83
AlexPlom <alexiosplomaritis@gmail.com>
2018-08-16 14:36:10 +0300

    Changes the way we build the client

---
+++ src/PushNotifications.Client.Net/Models/StatCount.cs
@@ -0,0 +1,9 @@
+﻿namespace PushNotifications.Api.Client
+{
+    public class StatCount
+    {
+        public long Count { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/PushNotifications.Client.Net/Models/SubscriberTokensModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PushNotifications.Api.Client
+{
+    public class SubscriberTokensModel
+    {
+        public SubscriberTokensModel(string subscriberUrn)
+        {
+            SubscriberUrn = subscriberUrn;
+        }
+
+        public string SubscriberUrn { get; private set; }
+    }
+}
